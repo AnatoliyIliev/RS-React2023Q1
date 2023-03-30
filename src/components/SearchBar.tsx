bd4953cec57d865496d2044ee55c62d79c410f82
AnatoliyIliev <vekok12@gmail.com>
2023-03-30 23:21:12 +0300

    feat: implement hooks to SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,70 +1,53 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from '../styles/SearchBar.module.scss';
 
-class SearchBar extends Component {
-  state = {
-    searchQuery: '',
-    err: false,
-  };
+function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState(localStorage.getItem('query') ?? '');
 
-  componentDidMount() {
-    const query = localStorage.getItem('query');
-    const parseQuery = JSON.parse(query!);
+  const [err, setErr] = useState(false);
 
-    if (parseQuery) {
-      this.setState({ searchQuery: parseQuery });
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('query', searchQuery);
+  }, [searchQuery]);
 
-  componentWillUnmount() {
-    const { searchQuery } = this.state;
-
-    localStorage.setItem('query', JSON.stringify(searchQuery));
-  }
-
-  handleSubmit = (event: React.SyntheticEvent) => {
+  const handleSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    const { searchQuery } = this.state;
-
-    this.setState({ err: false });
+    setErr(false);
 
     if (searchQuery.trim() === '') {
-      this.setState({ err: true });
+      setErr(true);
+
       return;
     }
-
-    this.setState({ searchQuery: '' });
+    setSearchQuery('');
   };
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const currentSearch = event.currentTarget.value.toLowerCase();
-    this.setState({ searchQuery: currentSearch });
+
+    setSearchQuery(currentSearch);
   };
 
-  render() {
-    const { searchQuery, err } = this.state;
-
-    return (
-      <>
-        <form onSubmit={this.handleSubmit} className={styles.SearchForm}>
-          <input
-            className={styles.SearchForm_input}
-            type="text"
-            value={searchQuery}
-            onChange={this.handleChange}
-            autoComplete="off"
-            autoFocus
-            placeholder={'Search'}
-          />
-          <button type="submit" className={styles.SearchForm_button}>
-            Search
-          </button>
-        </form>
-        {err && <div style={{ color: 'red', textAlign: 'center' }}>Enter something to search.</div>}
-      </>
-    );
-  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={styles.SearchForm}>
+        <input
+          className={styles.SearchForm_input}
+          type="text"
+          value={searchQuery}
+          onChange={handleChange}
+          autoComplete="off"
+          autoFocus
+          placeholder={'Search'}
+        />
+        <button type="submit" className={styles.SearchForm_button}>
+          Search
+        </button>
+      </form>
+      {err && <div style={{ color: 'red', textAlign: 'center' }}>Enter something to search.</div>}
+    </>
+  );
 }
 
 export default SearchBar;
