6438026df574a933f2d0985d84c30b66854e9f2a
AnatoliyIliev <vekok12@gmail.com>
2023-04-03 01:04:22 +0300

    feat: add tests Form and FormCards

--- src/test/Form.test.tsx
+++ src/test/Form.test.tsx
@@ -1,9 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, vi } from 'vitest';
 import Form from '../components/Form';
 
 describe('Forms', () => {
-  it('name ', () => {
+  const onSubmitForm = vi.fn();
+
+  beforeEach(() => {
+    onSubmitForm.mockClear();
+  });
+
+  it('should render all form fields and a submit button', () => {
     render(<Form />);
 
     expect(screen.getByText('Name:*')).toBeInTheDocument();
@@ -13,5 +19,32 @@ describe('Forms', () => {
     expect(screen.getByText('Pick your favorite genre:*')).toBeInTheDocument();
     expect(screen.getByText('Avatar:*')).toBeInTheDocument();
     expect(screen.getByText('I consent to my personal data*')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Submit' })).toBeInTheDocument();
+  });
+
+  it('should submit the form successfully', async () => {
+    render(<Form onSubmitForm={onSubmitForm} />);
+    fireEvent.input(screen.getByPlaceholderText('Name'), {
+      target: {
+        value: 'Alex',
+      },
+    });
+    fireEvent.input(screen.getByPlaceholderText('Phome number'), {
+      target: {
+        value: '1234567890',
+      },
+    });
+    fireEvent.input(screen.getByLabelText('Birth Date:*'), {
+      target: {
+        value: '1990-01-01',
+      },
+    });
+    fireEvent.change(screen.getByLabelText('Pick your favorite genre:*'), {
+      target: {
+        value: 'action',
+      },
+    });
+
+    fireEvent.click(screen.getByRole('button', { name: 'Submit' }));
   });
 });
--- src/test/FormCards.test.tsx
+++ src/test/FormCards.test.tsx
@@ -1,9 +1,46 @@
 import { render } from '@testing-library/react';
 import FormCards from '../components/FormCards';
+import { CardForm } from '../types';
 
 describe('FormCard', () => {
-  it('renders nothing', () => {
-    const { container } = render(<FormCards />);
-    expect(container.getElementsByClassName('.cards').length).toEqual(0);
+  it('renders form data correctly', () => {
+    const cards: CardForm[] = [
+      {
+        id: 1,
+        name: 'Alex',
+        phone: 1234567890,
+        date: '01/01/1990',
+        gender: 'Male',
+        genre: 'Action',
+        file: 'https://example.com/avatar.jpg',
+        agree: true,
+      },
+      {
+        id: 2,
+        name: 'Dima',
+        phone: 9876543210,
+        date: '02/02/1995',
+        gender: 'Female',
+        genre: 'Comedy',
+        file: 'https://example.com/avatar2.jpg',
+        agree: false,
+      },
+    ];
+
+    const { getByText } = render(<FormCards formCards={cards} />);
+
+    expect(getByText('Name: Alex')).toBeInTheDocument();
+    expect(getByText('Phone Number: 1234567890')).toBeInTheDocument();
+    expect(getByText('Birth Date: 01/01/1990')).toBeInTheDocument();
+    expect(getByText('Gender: Male')).toBeInTheDocument();
+    expect(getByText('My favorite genre: Action')).toBeInTheDocument();
+    expect(getByText('I consent to my personal data: Yes')).toBeInTheDocument();
+
+    expect(getByText('Name: Dima')).toBeInTheDocument();
+    expect(getByText('Phone Number: 9876543210')).toBeInTheDocument();
+    expect(getByText('Birth Date: 02/02/1995')).toBeInTheDocument();
+    expect(getByText('Gender: Female')).toBeInTheDocument();
+    expect(getByText('My favorite genre: Comedy')).toBeInTheDocument();
+    expect(getByText('I consent to my personal data: No')).toBeInTheDocument();
   });
 });
